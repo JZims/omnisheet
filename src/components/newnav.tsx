2ec9cfef15a9a1583dd446172582c8a47aba8a60
JZims <zimmerman.j@hotmail.com>
2023-08-28 17:16:38 -0400

    Should build

--- src/components/newnav.tsx
+++ src/components/newnav.tsx
@@ -7,7 +7,7 @@ import { SignOutButton, useUser} from "@clerk/nextjs";
 
   
 
-function classNames(...classes) {
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
